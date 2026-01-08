1cb5aa3aebf6c67457ec6da3637da75c752194f5
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-07 16:04:45 -0800

    added error pages

--- utils/serverStatic.js
+++ utils/serverStatic.js
@@ -9,13 +9,24 @@ export const serveStatic = async (req, res, dir) => {
     publicDir, 
     req.url === "/" ? "index.html" : req.url
   );
-  
   const ext = path.extname(filePath);
   const contentType = getContentType(ext);
+
   try {
     const content = await fs.readFile(filePath);
     await sendResponse(res, 200, contentType, content)
   } catch(err) {
     console.error(err)
+    let content;
+    let errCode;
+    if(err.code === 'ENOENT') {
+      const errorPath = path.join(publicDir, "404.html");
+      content = await fs.readFile(errorPath);
+      errCode = 404;
+    } else {
+      content = `<html><h1>Server Error: ${err.code}</h1></html>`
+      errCode = 500;
+    }
+    await sendResponse(res, errCode, 'text/html', content)
   }
 }
